e70e6a9e4b1897411abf2140facf3d4327a23831
nRookie <naqing19950908@gmail.com>
2022-11-06 00:17:53 +0800

    fix jsxImport not working issues

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     react({
-      jxsImportSource: '@emotion/react',
+      jsxImportSource: '@emotion/react',
       babel: {
         plugins : ['@emotion/babel-plugin'],
       },
