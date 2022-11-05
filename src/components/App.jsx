f8daa3e944586d135e716bf9e14bd2a15529baf6
nRookie <naqing19950908@gmail.com>
2022-11-06 00:02:43 +0800

    init commit

---
+++ src/components/App.jsx
@@ -0,0 +1,129 @@
+import React, {useState, useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {css} from '@emotion/react';
+import AdminContext from './AdminContext';
+
+import KanbanBoard, { 
+  COLUMN_KEY_DONE,
+  COLUMN_KEY_ONGOING,
+  COLUMN_KEY_TODO,
+} from './KanbanBoard';
+
+export const kanbanCardStyles = css`
+  margin-bottom: 1rem;
+  padding: 0.6rem 1rem;
+  border: 1px solid gray;
+  border-radius: 1rem;
+  list-style: none;
+  background-color: rgba(255, 255, 255, 0.4);
+  text-align: left;
+
+  &:hover {
+    box-shadow: 0 0.2rem 0.2rem rgba(0, 0, 0, 0.2), inset 0 1px #fff;
+  }
+`;
+export const kanbanCardTitleStyles = css`
+  min-height: 3rem;
+`;
+
+export const MINUTE = 60 * 1000;
+export const HOUR = 60 * MINUTE;
+export const DAY = 24 * HOUR;
+export const UPDATE_INTERVAL = MINUTE;
+
+const DATA_STORE_KEY = 'kanban-data-store';
+
+function App() {
+  const [todoList, setTodoList] = useState ( [
+    {title: '开发任务-1', status: '22-05-22 18:15' },
+    { title: '开发任务-3', status: '22-05-22 18:15' }, 
+    { title: '开发任务-5', status: '22-05-22 18:15' },
+    { title: '测试任务-3', status: '22-05-22 18:15' }
+  ]);
+
+  const [ongoingList, setOngoingList] = useState ( [
+    { title: '开发任务-4', status: '2022-05-22 18:15' },
+    { title: '开发任务-6', status: '2022-05-22 18:15' },
+    { title: '测试任务-2', status: '2022-05-22 18:15' }
+  ]);
+  const [doneList, setDoneList] = useState ( [
+    { title: '开发任务-2', status: '2022-05-22 18:15' },
+    { title: '测试任务-1', status: '2022-05-22 18:15' }
+  ]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const updaters = {
+    [COLUMN_KEY_TODO]: setTodoList,
+    [COLUMN_KEY_ONGOING]: setOngoingList,
+    [COLUMN_KEY_DONE] : setDoneList,
+  }
+
+
+
+  const handleAdd = (column, newCard) => {
+    updaters[column]((currentStat) => [newCard, ...currentStat]);
+  };
+
+  const handleRemove = (column, cardToRemove) => { 
+    updaters[column]((currentStat) => 
+    currentStat.filter((item) => item.title !== cardToRemove.title) 
+    ); 
+  };
+
+  useEffect(() => {
+    const data = window.localStorage.getItem(DATA_STORE_KEY);
+    setTimeout(() => { 
+      if (data) {
+        const kanbanColumnData = JSON.parse(data);
+        setTodoList(kanbanColumnData.todoList);
+        setOngoingList(kanbanColumnData.ongoingList);
+        setDoneList(kanbanColumnData.doneList);
+      }
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+
+  const handleSaveAll = () => {
+    const data = JSON.stringify({
+      todoList,
+      ongoingList,
+      doneList
+    });
+    window.localStorage.setItem(DATA_STORE_KEY, data);
+  }
+
+
+  const [isAdmin, setIsAdmin] = useState(false); 
+  const handleToggleAdmin = (evt) => { 
+    setIsAdmin(!isAdmin); 
+   };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>我的看板 
+          <button onClick={handleSaveAll}>保存所有卡片</button>
+          <label>
+            <input type="checkbox" value={isAdmin} onChange={handleToggleAdmin} ></input>
+            管理员模式
+          </label>
+        </h1>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <AdminContext.Provider value={isAdmin}>
+        <KanbanBoard
+          isLoading={isLoading}
+          todoList={todoList}
+          ongoingList={ongoingList}
+          doneList={doneList}
+          onAdd={handleAdd}
+          onRemove={handleRemove}
+          />
+      </AdminContext.Provider>
+    </div>
+    );
+};
+
+export default App;
---
+++ src/components/KanbanCard.jsx
@@ -0,0 +1,50 @@
+import React, {useContext, useState, useEffect } from 'react';
+import { css } from '@emotion/react';
+import { MINUTE, HOUR, DAY, UPDATE_INTERVAL, kanbanCardStyles, kanbanCardTitleStyles } from './App';
+import AdminContext from './AdminContext';
+
+export default function KanbanCard({ title, status, onDragStart, onRemove }) {
+
+  const [displayTime, setDisplayTime] = useState(status);
+  useEffect(() => {
+    const updateDisplayTime = () => {
+      const timePassed = new Date() - new Date(status);
+      let relativeTime = '刚刚';
+      if (MINUTE <= timePassed && timePassed < HOUR) {
+        relativeTime = `${Math.ceil(timePassed / MINUTE)} 分钟前`;
+      } else if (HOUR <= timePassed && timePassed < DAY) {
+        relativeTime = `${Math.ceil(timePassed / HOUR)} 小时前`;
+      } else if (DAY <= timePassed) {
+        relativeTime = `${Math.ceil(timePassed / DAY)} 天前`;
+      }
+      setDisplayTime(relativeTime);
+    };
+    const intervalId = setInterval(updateDisplayTime, UPDATE_INTERVAL);
+    updateDisplayTime();
+
+    return function cleanup() {
+      clearInterval(intervalId);
+    };
+  }, [status]);
+
+
+  const handleDragStart = (evt) => {
+    evt.dataTransfer.effectAllowed = 'move';
+    evt.dataTransfer.setData('text/plain', title);
+    onDragStart && onDragStart(evt);
+  };
+  const isAdmin = useContext(AdminContext);
+
+  return (
+    <li css={kanbanCardStyles} draggable onDragStart={handleDragStart}>
+      <div css={kanbanCardTitleStyles}>{title}</div>
+      <div css={css`
+      `} title={status}>{displayTime} {isAdmin && onRemove && (
+        <button onClick={() => onRemove({title})}>X</button>
+      )}</div>
+    </li>
+  );
+}
+
+
+ 
